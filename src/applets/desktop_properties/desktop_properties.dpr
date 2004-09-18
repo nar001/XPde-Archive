9494e5fd56db359c8e8248217a244cec77293551
Jose Leon Serna <ttm@users.sourceforge.net>
2004-09-18 15:06:09 +0000

    Initial revision

---
+++ src/applets/desktop_properties/desktop_properties.dpr
@@ -0,0 +1,21 @@
+program desktop_properties;
+
+uses
+  QForms,
+  QThemed in '../../common/QTheming/QThemed.pas',
+  uDisplayProperties in 'uDisplayProperties.pas' {DisplayPropertiesDlg},
+  uXPColorSelector in '../../components/XPColorSelect/uXPColorSelector.pas',
+  uXPColorDialog in '../../components/XPColorSelect/uXPColorDialog.pas' {XPColorDialog},
+  uRegistry in '../../components/XPRegistry/uRegistry.pas',
+  uRegLib in '../../components/XPRegistry/uRegLib.pas',
+  uXPIPC in '../../common/uXPIPC.pas';
+
+{$R *.res}
+
+begin
+
+  Application.Initialize;
+  Application.CreateForm(TDisplayPropertiesDlg, DisplayPropertiesDlg);
+  Application.CreateForm(TXPColorDialog, XPColorDialog);
+  Application.Run;
+end.
